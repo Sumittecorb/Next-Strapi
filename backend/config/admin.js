3eca38148e4f834600cbde1b6fa3159b01078251
Sumit Saxena <sumit.2019@tecorb.co>
2023-05-30 15:24:31 +0530

    First pus

---
+++ backend/config/admin.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+});
---
+++ frontend/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  // ab76658c36064bdae7258f718ac7a3210fafdba527ec671f6e2b6054ee7dd5efdc49ca412a1a6f87be8bb7c54e5f4017c04fc27580004dca47ba15cb7e505e162e8983da673f6c9466e3553bfeadd3e7d77cd030ebc8d3d03a282d7b66138025da398056074aaf6e94300175648588204871793d562216c4441486badb5d71ce
+  return <Component {...pageProps} />
+}
+
+export default MyApp
