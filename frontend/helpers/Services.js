d42e860dda387246612cf0041294695a3dba1e7c
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-06-01 12:00:35 +0530

    tailwind config and product mapping

--- frontend/helpers/Services.js
+++ frontend/helpers/Services.js
@@ -7,9 +7,9 @@ export const listing_resource = async () => {
     let response = await Http.get(API_URL.LOREM_IPSUM, {})
     return response
 }
-export const listing_postType = async (data) => {
-    console.log("data",data)
-    let response = await Http.post(API_URL.Gaurav, {...data},{})
-    return response
-}
+// export const listing_postType = async (data) => {
+//     console.log("data",data)
+//     let response = await Http.post(API_URL.Gaurav, {...data},{})
+//     return response
+// }
 // console.log("data",response)
--- frontend/helpers/api_url.js
+++ frontend/helpers/api_url.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = "http://54.183.175.217:3000/";
-export const BASE_URL = 'https://jsonplaceholder.typicode.com'
+export const BASE_URL = 'https://fakestoreapi.com'
 // export const VERSION = "/api/v1/";
 export const API_URL = {
- LOREM_IPSUM:"/posts",
+ LOREM_IPSUM:"/products",
  Gaurav:"/posts"
 }
