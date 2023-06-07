0a5e09483211fae4455d73a5657f4a920cfa91ae
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-06-07 12:36:18 +0530

    merge code with main branch

--- frontend/components/Cards/ProductCard.jsx
+++ frontend/components/Cards/ProductCard.jsx
@@ -4,27 +4,22 @@ import { useDispatch } from "react-redux";
 import { add, remove } from "../../store/cartSlice";
 import { useAppSelector } from "../../store/hooks";
 
-const ProductCard = ({ itemData, type,  onClick }) => {
+const ProductCard = ({ itemData, type, onClick }) => {
   const {
     id,
     addedToCart,
     attributes: { title, price, img },
   } = itemData;
-  // console.log("itemData-===========", itemData)
-  const products = useAppSelector(state => state.cart)
-  // console.log(products,"products");
-
-
-
+  // const products = useAppSelector((state) => state.cart);
+ 
 
   const { push } = useRouter();
   const dispatch = useDispatch();
 
-
   const handleRemove = (_id) => {
     dispatch(remove(_id));
   };
-  
+
   return (
     <div
       key={`demo${id}`}
@@ -43,25 +38,31 @@ const ProductCard = ({ itemData, type,  onClick }) => {
         <p className="text-gray-700 text-center text-2xl">{price}</p>
       </div>
 
-          
-
       <div className="px-6 pt-4 pb-2 flex items-center justify-center">
-        {type == "product" ?(
-             id && addedToCart ? 
-             <button 
-              onClick={()=>push('/cart')}
-              className="bg-sky-400 rounded-xl text-pink-500 px-3 py-2" >
-              Goto Cart</button> 
-              :
-           <button onClick={() => onClick(itemData)} className="bg-emerald-200 rounded-xl text-green-900 px-3 py-2">
-            Add to cart
-          </button>
+        {type == "product" ? (
+          id && addedToCart ? (
+            <button
+              onClick={() => push("/cart")}
+              className="bg-sky-400 rounded-xl text-pink-500 px-3 py-2"
+            >
+              Goto Cart
+            </button>
+          ) : (
+            <button
+              onClick={() => onClick(itemData)}
+              className="bg-emerald-200 rounded-xl text-green-900 px-3 py-2"
+            >
+              Add to cart
+            </button>
           )
-  
-         :
-          <button onClick={() => handleRemove(id)} className="bg-emerald-200 rounded-xl text-green-500 px-3 py-2">
+        ) : (
+          <button
+            onClick={() => handleRemove(id)}
+            className="bg-emerald-200 rounded-xl text-green-500 px-3 py-2"
+          >
             Remove Item
-          </button>} 
+          </button>
+        )}
       </div>
     </div>
   );
