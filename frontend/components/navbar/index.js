f8e4155e9935170a801abee1ea8256909ed97fb2
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-06-08 18:42:58 +0530

    dropdown,searchbar

--- frontend/components/navbar/index.js
+++ frontend/components/navbar/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useAppSelector } from "../../store/hooks";
+
 export default function Navbar() {
   const items = useAppSelector((state) => state.cart);
   return (
@@ -7,17 +8,22 @@ export default function Navbar() {
       <nav className="nav flex flex-wrap items-center justify-between px-4 bg-teal-400 py-3 ">
         <div className="flex flex-no-shrink items-center mr-6 py-3 text-grey-darkest">
           <span className="font-semibold text-xl tracking-tight">
-            Redux Store
+            <Link legacyBehavior href={"/"}>
+              <a className="block md:inline-block px-4 py-3 no-underline text-grey-darkest hover:text-grey-darker cursor-pointer">
+                Redux Store
+              </a>
+            </Link>
           </span>
         </div>
 
-        <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
-        <label
-          className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none"
-          htmlFor="menu-btn"
-        >
-          <span className="navicon bg-grey-darkest flex items-center relative"></span>
-        </label>
+        <div className="flex flex-no-shrink items-center mr-6 py-3 text-grey-darkest">
+          <span className="font-semibold text-2xl tracking-tight">
+            <a className="block md:inline-block px-4 py-3 no-underline text-grey-darkest hover:text-grey-darker cursor-pointer">
+              Welcome to the Redux Store
+            </a>
+          </span>
+        </div>
+
         <ul className="menu border-b md:border-none flex justify-end list-reset m-0 w-full md:w-auto">
           <li className="border-t md:border-none">
             <Link legacyBehavior href={"/"}>
